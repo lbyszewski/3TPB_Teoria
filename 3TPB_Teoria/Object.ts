5e80f5826778640ff1430c5ad2b5cbf5e974b792
inz Lukasz Byszewski <gdx153483@wsb.gda.pl>
2023-05-22 21:50:33 +0200

    Typescript update for learning

--- 3TPB_Teoria/Object.ts
+++ 3TPB_Teoria/Object.ts
@@ -3,29 +3,29 @@ export class Something {
     // property  pierwszej wartości
     private _firstValue: number;
 
-    public get gFirstValue(): number {
+    public getFirstValue(): number {
         return this._firstValue;
     }
-    public set sFirstValue(value: number) {
+    public setFirstValue(value: number) {
         this._firstValue = value;
     }
     // property  drugiej wartości
     private _secondValue: number;
 
-    public get gSecondValue(): number {
+    public getSecondValue(): number {
         return this._secondValue;
     }
-    public set sSecondValue(value: number) {
+    public setSecondValue(value: number) {
         this._secondValue = value;
     }
     // property  trzeciej wartości
     private _thirdValue: number;
 
 
-    public get gThirdValue(): number {
+    public getThirdValue(): number {
         return this._thirdValue;
     }
-    public set sThirdValue(value: number) {
+    public setThirdValue(value: number) {
         this._thirdValue = value;
     }
 
@@ -38,7 +38,7 @@ export class Something {
 
     // metoda zwracana 
     public doSomthingWithValues(): number {
-       let result =  this.gFirstValue * this.gSecondValue * this.gThirdValue;
+       let result =  this.getFirstValue() * this.getSecondValue() * this.getThirdValue();
        return result;
     }
 
